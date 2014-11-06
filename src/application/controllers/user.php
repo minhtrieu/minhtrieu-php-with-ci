7bf37f2c3d7d4858fad7fdacecb62cb78fef3bd1
minhtrieu <ttminh_111@yahoo.com>
2014-11-06 19:46:45 +0700

    update model to match with pingo

--- src/application/controllers/user.php
+++ src/application/controllers/user.php
@@ -1,124 +1,64 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
 class User extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->load->helper('url');
+        $this->load->model("PingoModel");
+    }
+
     public function register()
     {
+        $this->load->model("Users", "userModel");
+        $errorMessage = '';
         if (isset($_POST['btnRegister'])) {
             // Processing registering new account
             $email = $this->input->post('txtEmail');
             $this->load->helper('email');
-            if (valid_email($email)) {
-                //check if email is in database
-                $this->load->database();
-                $this->load->model('my_model');
-                $sql = array(
-                    'email' => $email
-                );
-                $query = $this->my_model->get_user($sql);
-                if (empty($query)) {
-                    $email_valid = 1;
-                } else {
-                    $this->register_err("Email already existed",$email); //wrong email
-                }
-
-            } else {
-               $email_valid=  0;
+            if (!valid_email($email)) {
+                $errorMessage =  'email is invalid';
+                $this->load->view("layout/layout", array(
+                    'errorMessage'  => $errorMessage,
+                    'mainContent'   => VIEW_PATH . '/user/register.php'
+                    ));
+                return;
             }
 
             $password = $this->input->post('txtPassword');
-            $confirmPassword =  $this->input->post('txtConfirmPassword');
-            if ($password === $confirmPassword) {
-                if ($email_valid == 1) {
-                    //put information to database
-                    $this->load->model ('my_model');
-                    $sql = array($email,$password);
-                    $this->my_model->add_user($sql);
-                    $this->login(); //success
-                } else {
-                    $this->register_err("Email format must be yourname@something.com",""); //wrong email
-                }
+            $confirmPassword = $this->input->post('txtConfirmPassword');
+
+            // Kiem tra email form name@domain.com
+            // So sanh password voi confirm password
+            // Kiem tra email co ton tai 
+            // Password > 6 ky tu
+            $user = array(
+                'email'         => $email,
+                'password'  => $password
+            );
+
+            $userId = $this->userModel->createUser($user);
+
+            if ($userId === false) {
+                $errorMessage = "Can not create new user. Please try again";
             } else {
-                $this->register_err("Password is mismatch",$email);
+                return $this->login();    
             }
-            return $this->login();
-        } elseif (isset($_POST['btnLogin'])) {
-            return $this->login();
         }
         $this->load->view("layout/layout", array(
-            'mainContent' => VIEW_PATH . '/user/register.php'
-        ));
-
-
-
+            'errorMessage'  => $errorMessage,
+            'mainContent'   => VIEW_PATH . '/user/register.php'
+            ));
     }
 
-    private function register_err ($err_msg, $email)
-    {
-        $this->load->view("layout/layout", array(
-            'mainContent' => VIEW_PATH.'/user/register.php',
-            'err_msg' => $err_msg,
-            'email'   => $email
-        ));
-    }
-
-    private function login_err ($err_msg, $email)
-    {
-        $this->load->view("layout/layout", array(
-            'mainContent' => VIEW_PATH.'/user/login.php',
-            'err_msg' => $err_msg,
-            'email'   => $email
-        ));
-
-    }
     function login()
     {
-//        var_dump($_POST);
-        if (isset($_POST['btnLogin'])) {
-            $email = $this->input->post('txtEmail');
-            $password = $this->input->post("txtPassword");
-            $this->load->helper('email');
-            if (valid_email($email)) {
-
-                //compare password in database
-                $this->load->database();
-                $this->load->model ('my_model');
-                $sql = array(
-                    'email' => $email
-                );
-                $query = $this->my_model->get_user($sql);
-                $sql_row = $query[0];
-                if ($password === $sql_row['password']) {
-                    $_SESSION['login'] =1;
-                    $this->load->view("layout/layout", array(
-                        'mainContent'   => VIEW_PATH . '/layout/left_content.php'
-                    ));
-
-                } else {
-                    $this->login_err("Invalid Password",$email);
-                }
-            } else {
-                $this->login_err('Invalid Email',$email);
-            }
-
-        return  $this->load->view("layout/layout", array(
-            'mainContent'   => VIEW_PATH . '/layout/left_content.php'
-            ));
-        }
-
         $this->load->view("layout/layout", array(
             'mainContent'   => VIEW_PATH . '/user/login.php'
-        ));
-
-    }
-
-    function logout ()
-    {
-        $_SESSION['login'] = ''; //for removing PHP message
-        unset($_SESSION['login']);
-        $this->load->view("layout/layout", array(
-            'mainContent'   => VIEW_PATH . '/layout/left_content.php'
-        ));
+            ));
     }
 }
 
-
-
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
